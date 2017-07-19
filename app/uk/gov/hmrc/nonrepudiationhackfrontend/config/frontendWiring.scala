1a5bc69ace64a24a6464df1e3a4a259a09378367
Francis Devereux <francis.devereux@digital.hmrc.gov.uk>
2017-07-19 11:09:31 +0100

    Remove unneeded auth library

--- app/uk/gov/hmrc/nonrepudiationhackfrontend/config/frontendWiring.scala
+++ app/uk/gov/hmrc/nonrepudiationhackfrontend/config/frontendWiring.scala
@@ -19,7 +19,6 @@ package uk.gov.hmrc.nonrepudiationhackfrontend
 import uk.gov.hmrc.play.audit.http.config.LoadAuditingConfig
 import uk.gov.hmrc.play.audit.http.connector.{AuditConnector => Auditing}
 import uk.gov.hmrc.play.config.{AppName, RunMode, ServicesConfig}
-import uk.gov.hmrc.play.frontend.auth.connectors.AuthConnector
 import uk.gov.hmrc.play.http.ws.{WSDelete, WSGet, WSPost, WSPut}
 
 object FrontendAuditConnector extends Auditing with AppName {
@@ -28,9 +27,4 @@ object FrontendAuditConnector extends Auditing with AppName {
 
 object WSHttp extends WSGet with WSPut with WSPost with WSDelete with AppName with RunMode {
   override val hooks = NoneRequired
-}
-
-object FrontendAuthConnector extends AuthConnector with ServicesConfig {
-  val serviceUrl = baseUrl("auth")
-  lazy val http = WSHttp
 }
