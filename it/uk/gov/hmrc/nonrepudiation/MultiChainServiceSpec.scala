b407d7b7d14745cfcca3098da43f2e4bdac3414f
Francis Devereux <francis.devereux@digital.hmrc.gov.uk>
2017-07-19 13:25:34 +0100

    MultiChainServiceSpec cleanup

--- it/uk/gov/hmrc/nonrepudiation/MultiChainServiceSpec.scala
+++ it/uk/gov/hmrc/nonrepudiation/MultiChainServiceSpec.scala
@@ -18,33 +18,23 @@ package uk.gov.hmrc.nonrepudiation
 
 import java.util.UUID
 
-import org.scalatest.concurrent.Eventually
 import play.api.test.WsTestClient
 import uk.gov.hmrc.play.test.UnitSpec
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration._
 
-class MultiChainServiceSpec extends UnitSpec with WsTestClient with Eventually {
-  override implicit def patienceConfig: PatienceConfig = PatienceConfig(timeout = 10 seconds)
-
-//  "publishEvent" should {
-//    "publish event to the blockhain" in {
-//      val multiChainService = new MultiChainService()
-//      multiChainService.publishEvent(Event("Hello world!"))
-//    }
-//  }
+class MultiChainServiceSpec extends UnitSpec with WsTestClient {
 
   "list" should {
     "list published events" in {
       withClient { wsClient =>
         val uniqueString = UUID.randomUUID().toString
         val multiChainService = new MultiChainService(wsClient, "http://localhost:4272", "multichainrpc", "CoxcdVSWVBP9TR52V3Kmddm8jvzmNHn1VPPWfca6dSQb", "streamX")
+
         await(multiChainService.publishEvent(Event(uniqueString)))
-        eventually {
-          val events: Seq[Event] = await(multiChainService.list())
-          events should contain(Event(uniqueString))
-        }
+
+        val events: Seq[Event] = await(multiChainService.list())
+        events should contain(Event(uniqueString))
       }
     }
   }
